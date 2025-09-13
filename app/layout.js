270caa0801a7c90898b7048f9193efd720117317
Rajkumar-robert <rajkumar09112003@gmail.com>
2025-09-13 23:47:25 +0530

    Feat: Wrap children in UserContextProvider and add Navbar component

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import UserContextProvider from "./context/userContext";
+import Navbar from "./components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,7 +24,10 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased cz-shortcut-listen="true"`}
       >
-        {children}
+        <UserContextProvider>
+          <Navbar />
+          {children}
+        </UserContextProvider>
       </body>
     </html>
   );
